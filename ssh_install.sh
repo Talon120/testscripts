61a7066f757c939a10711d31153fccb4811f461a
Talon120 <taloncjones@gmail.com>
2016-10-15 04:07:32 +1030

    Created ssh_install files to ssh into IPs listed in ipconfig and install iPerf3 and dependent libraries. Running ssh_install.sh will call ssh_install for each IP.

---
+++ ssh_install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Program name:	ssh_install.sh
+# Created by:	Talon Jones
+# Created:	12 Oct. 2016
+# Updated:	12 Oct. 2016
+# Purpose:	Automate ssh_install for every board/IP listed in [filename].
+# Usage:	./ssh_install.sh
+
+filename="ipconfig.txt"
+
+echo "Enter device password:"
+read pw
+
+readarray -t boardIP < $filename
+
+#boardIP=$(awk '{printf $2}' $filename)
+
+for e in "${boardIP[@]}"
+do
+	e=$(echo "$e" | awk '{printf $2}')
+	echo "$e"
+	./ssh_install $e $pw
+done
