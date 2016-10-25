60a627769329648b16a38d2d00ca3e30c2a1b5b8
Talon120 <Talon120@github.com>
2016-10-26 06:31:10 +1030

    Updated for new ipconfig format, formatting, and updated dates.

--- setup_iperf.sh
+++ setup_iperf.sh
@@ -3,10 +3,9 @@
 # Program name:	setup_iperf.sh
 # Created by:	Talon Jones
 # Created:	29 Sept. 2016
-# Updated:	18 Oct. 2016
+# Updated:	25 Oct. 2016
 # Purpose:	Bash script to install iperf3 and all dependent libraries.
 # Note:		iPerf directory will be cloned to current directory.
-
 
 echo 'y' | sudo apt-get update
 echo 'y' | sudo apt-get install git make expect
--- ssh_install.sh
+++ ssh_install.sh
@@ -3,7 +3,7 @@
 # Program name:	ssh_install.sh
 # Created by:	Talon Jones
 # Created:	12 Oct. 2016
-# Updated:	12 Oct. 2016
+# Updated:	25 Oct. 2016
 # Purpose:	Automate ssh_install for every board/IP listed in [filename].
 # Usage:	./ssh_install.sh
 
@@ -18,7 +18,7 @@ readarray -t boardIP < $filename
 
 for e in "${boardIP[@]}"
 do
-	e=$(echo "$e" | awk '{printf $2}')
+	e=$(echo "$e" | awk '{printf $3}')
 	echo "$e"
 	./ssh_install $e $pw
 done
