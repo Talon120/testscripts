0c7c80575d8d4316525573c7ec4831d00eb2117a
Talon120 <Talon120@github.com>
2016-11-10 11:33:28 +1030

    Progress on get_logs and get_logs.sh

--- get_logs.sh
+++ get_logs.sh
@@ -1,3 +1,43 @@
-#!/bin/sh
+#!/bin/bash
 
-scp odroid@192.168.1.6:log* .
+# Program name:	get_logs.sh
+# Created by:	Talon Jones
+# Created:	3 Nov. 2016
+# Updated:	9 Nov. 2016
+# Purpose:	Retrieve any iperf log files from boards listed in ipconfig.txt.
+# Usage:	./get_logs.sh {board}
+
+filename="ipconfig.txt"
+board=$1
+mkdate=$(date +"%F_%H")
+
+echo "Enter device password:"
+read -s pw
+
+if [ -n "$board" ]; then
+	e=$(grep -w $board $filename)
+	initVIP=$(echo "$e" | awk '{printf $4}')
+	if [ -n "$initVIP" ]; then
+		mkdir -p $mkdate
+		echo "Running get_logs on $board."
+		./get_logs $initVIP $pw $board $mkdate
+		echo "Retrieved logs from $board."
+	else
+		echo "$board not found in $filename."
+	fi
+
+else
+	readarray -t boardIP < $filename
+	mkdir -p $mkdate
+
+	for e in "${boardIP[@]}"
+	do
+		initVIP=$(echo "$e" | awk '{printf $4}')
+		board=$(echo "$e" | awk '{printf $4}')
+		if [ "$initVIP" != "[VLAN" ]; then
+			echo "Running get_logs on $board."
+			./get_logs $initVIP $pw $board $mkdate
+			echo "Retrieved logs from $board."
+		fi
+	done
+fi
