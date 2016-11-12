69e742c917f45c207d0a5f70151be002e1d0c421
Talon120 <Talon120@github.com>
2016-11-12 10:57:41 +1030

    Began work on log parser. Purpose: To gather the summary data from all logs and make it easily readable.

--- get_logs.sh
+++ get_logs.sh
@@ -3,13 +3,24 @@
 # Program name:	get_logs.sh
 # Created by:	Talon Jones
 # Created:	3 Nov. 2016
-# Updated:	10 Nov. 2016
+# Updated:	11 Nov. 2016
 # Purpose:	Retrieve any iperf log files from boards listed in ipconfig.txt.
 # Usage:	./get_logs.sh {board}
 
 filename="ipconfig.txt"
 board=$1
 mkdate=$(date +"%F_%H")
+
+logParse() {
+	cd $mkdate
+	for logFolders in .
+	do
+		for logFiles in "$logFolders"/*
+		do
+			echo $logFiles
+		done
+	done
+}
 
 echo "Enter device password:"
 read -s pw
@@ -18,21 +29,22 @@ if [ -n "$board" ]; then
 	e=$(grep -w $board $filename)
 	initVIP=$(echo "$e" | awk '{printf $4}')
 	if [ -n "$initVIP" ]; then
-		mkdir -p $mkdate/log_test1/logs/
-        	mkdir -p $mkdate/log_test2/logs/
-	        mkdir -p $mkdate/log_test3/logs/
+		mkdir -p $mkdate/log_test1/
+        	mkdir -p $mkdate/log_test2/
+	        mkdir -p $mkdate/log_test3/
 		echo "Running get_logs on $board."
 		./get_logs $initVIP $pw $board $mkdate
 		echo "Retrieved logs from $board."
+		logParse
 	else
 		echo "$board not found in $filename."
 	fi
 
 else
 	readarray -t boardIP < $filename
-	mkdir -p $mkdate/log_test1/logs/
-	mkdir -p $mkdate/log_test2/logs/
-	mkdir -p $mkdate/log_test3/logs/
+	mkdir -p $mkdate/log_test1/
+	mkdir -p $mkdate/log_test2/
+	mkdir -p $mkdate/log_test3/
 
 	for e in "${boardIP[@]}"
 	do
@@ -44,5 +56,6 @@ else
 			echo "Retrieved logs from $board."
 		fi
 	done
+	logParse
 fi
 
