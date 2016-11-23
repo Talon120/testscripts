f4615ceacad0cfc49de4e15e6ade4e6aaba43662
Talon120 <Talon120@github.com>
2016-11-24 10:23:17 +1030

    ipconfig.txt updated for IP change in test configuration. test_iperf updtated: 	Server will now bind the server port to the LAN IP address given. This prevents iperf from binding the server port to the VLAN IP address to prevent limiting bandwidth by internal bridging. test_transfer and test_transfer.sh 	Will initiate a series of file transfers to ensure accurate data transfers. Server will first download a control file, then initiate the download on board1, transfer from board1 to board2, transfer from board2 to the server, and finally do a comparison between the control and the transfered file.

---
+++ test_transfer.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#
+# Program name:	test_iperf.sh
+# Created by:	Talon Jones
+# Created:	22 Nov. 2016
+# Purpose:	Automate the file transfer test between the central server and two test boards.
+
+
+filename="ipconfig.txt"
+board="Odroid"
+
+echo "This test will transfer the designated file from: Server->Board1->Board2->Server"
+echo "Enter first board #: "
+read boardC
+echo "Enter second board #: "
+read boardS
+echo "Enter password: [Note: password needs to be the same for both boards]"
+read pw
+
+boardC="$board$boardC"
+boardS="$board$boardS"
+
+echo -e "\nClient board lookup: $boardC     Server board lookup: $boardS"
+
+boardCLanIP=$(grep -w $boardC $filename | awk '{printf $2}')
+boardSLanIP=$(grep -w $boardS $filename | awk '{printf $2}')
+boardCVlanIP=$(grep -w $boardC $filename | awk '{printf $3}')
+boardSVlanIP=$(grep -w $boardS $filename | awk '{printf $3}')
+
+echo -e "Client board Internal IP: $boardCLanIP     VLAN IP: $boardCVlanIP"
+echo -e "Server board Internal IP: $boardSLanIP     VLAN IP: $boardSVlanIP"
+
+echo -e "\nName of file to transfer: [1GB, 512MB, 200MB, 100MB, 50MB, 20MB, 10MB, 5MB]"
+read transferFile
+
+if [[ $(ls | grep "$transerFile.zip") ]]; then
+	echo -e "\n\nControl file found on server.\n"
+else
+	echo -e "\n\nControl file not found. Downloading now.\n"
+	wget "download.thinkbroadband.com/$transferFile.zip"
+fi
+
+./test_transfer $boardCLanIP $boardSLanIP $pw "$transferFile.zip" $boardCVlanIP $boardSVlanIP
+
+diff "$transferFile.zip" "$transferFile.zip.new"
+ret=$?
+
+if [[ $ret -eq 0 ]]; then
+	echo -e "No differences found between:\nControl File:\t$transferFile.zip\nTransfered File:\t$transferFile.zip.new\n"
+else
+	echo "Error: diff command failed with filenames given: $transferFile.zip and $transferFile.zip.new"
+fi
+
